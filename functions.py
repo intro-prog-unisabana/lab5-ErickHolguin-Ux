08ce5ea73aa24776c93b8f035befbca2609cc7ff
Erick Fernando Holguin Ceron <subtanicdos3@gmail.com>
2026-03-12 10:32:25 -0500

    functions 1

---
+++ functions.py
@@ -0,0 +1,9 @@
+def calcular_promedio(numeros):
+    return sum(numeros) / len(numeros)
+
+#numeros = input()
+#print(calcular_promedio())
+
+datos = [85, 92, 78]
+prom = calcular_promedio(datos)  
+print(prom)
