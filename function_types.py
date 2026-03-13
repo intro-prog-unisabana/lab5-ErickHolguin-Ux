1413b5f38b17d38470f908db31b6b2656d355d8a
Erick Fernando Holguin Ceron <subtanicdos3@gmail.com>
2026-03-13 08:35:33 -0500

    return_types

---
+++ function_types.py
@@ -0,0 +1,4 @@
+def modificar_datos(valor, incremento):
+    
+    for i in range(len(valor)):
+        valor[i] += incremento
--- return_types.py
+++ return_types.py
@@ -2,5 +2,3 @@ def precio_usuario():
     """valor con que debe retornar en float, un precio"""
     precio = input("Enter the item's price:\n")
     return float(precio)
-
-print(precio_usuario())
