bfbf7a6ad7f9c0beaa708a3bd6315025675db50c
Erick Fernando Holguin Ceron <subtanicdos3@gmail.com>
2026-03-13 09:16:18 -0500

    modules 1

---
+++ modules.py
@@ -0,0 +1,13 @@
+import math
+import os
+
+cwd = os.getcwd()
+print(f"Current working directory: {cwd}")
+
+
+num = int(input("Enter an integer: "))
+log_val = math.log2(num)
+print(f"Log base 2 of {num} is: {log_val}")
+
+print(f"Floor: {math.floor(log_val)}")
+print(f"Ceiling: {math.ceil(log_val)}")
