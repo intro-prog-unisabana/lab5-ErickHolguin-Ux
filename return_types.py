e2ee705e204332410794bbfe22038b03f4a7617f
Erick Fernando Holguin Ceron <subtanicdos3@gmail.com>
2026-03-13 08:27:28 -0500

    Retunr return_types

---
+++ return_types.py
@@ -0,0 +1,6 @@
+def precio_usuario():
+    """valor con que debe retornar en float, un precio"""
+    precio = input("Enter the item's price:\n")
+    return float(precio)
+
+print(precio_usuario())
