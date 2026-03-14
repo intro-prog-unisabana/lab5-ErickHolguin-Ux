dd8e2f20959b2a1e6159427445dc7b23ab710eca
Erick Fernando Holguin Ceron <subtanicdos3@gmail.com>
2026-03-14 11:22:56 -0500

    Main 1

---
+++ main.py
@@ -0,0 +1,40 @@
+from utils import add, sub, multiply, divide, exponent, modulo, floor_divide, absolute
+
+def main():
+    while True:
+        operacion = input("Which calculation would you like to perform? "
+        "(add, subtract, multiply, divide, exponent, modulo, floor_divide, absolute, exit):\n").lower()
+
+        if operacion == "exit":
+            break
+
+        if operacion not in ["add", "sub", "multiply", "divide", "exponent", "modulo", "floor_divide", "absolute"]:
+            print("Invalid option!")
+            continue
+
+        if operacion in ["add", "sub", "multiply", "divide", "exponent", "modulo", "floor_divide"]:
+            numero_1 = float(input("Enter the first number: \n"))
+            numero_2 = float(input("Enter the second number \n"))
+
+            if operacion == "add":
+                resultado = add(numero_1, numero_2)
+            elif operacion == "sub":
+                resultado = sub(numero_1, numero_2)
+            elif operacion == "multiply":
+                resultado = multiply(numero_1, numero_2)
+            elif operacion == "divide":
+                resultado = divide(numero_1, numero_2)
+            elif operacion == "exponente":
+                resultado = exponent(numero_1, numero_2)
+            elif operacion == "modulo":
+                resultado = modulo(numero_1, numero_2)
+            elif operacion == "floor_divide":
+                resultado = floor_divide(numero_1, numero_2)
+        elif operacion == "absolute":
+            numero = float(input("Enter the number:\n"))
+            resultado = absolute(numero)
+
+        print(f"The result is: {resultado}")
+
+if __name__ == "__main__":
+    main()
--- utils.py
+++ utils.py
@@ -46,46 +46,46 @@ if __name__ == "__main__":
   print("This file is being run directly.")
 # FREEZE CODE END
 
-def add(num1, num2):
+def add(numero_1, numero_2):
     """ Devuelve la suma de numero 1 y numero2. """
-    return num1 + num2
+    return numero_1 + numero_2
 
-def sub(num1, num2):
+def sub(numero_1, numero_2):
     """ Devuelve el resultado de restar numero 2 a numero 1."""
-    return num1 - num2
+    return numero_1 - numero_2
 
-def multiply(num1, num2):
+def multiply(numero_1, numero_2):
     """ Devuelve la multiplicacion de numero 1 y el numero 2. 
     int o float: resultado de la multiplicación. """
-    return num1 * num2
+    return numero_1 * numero_2
 
-def divide(num1, num2):
+def divide(numero_1, numero_2):
     """ Deuelve el resultado de dividir numero 1 entre el numero 2.
     rectifica que el numero 2 no sea cero """
-    if num2 == 0:
+    if numero_2 == 0:
         return "Error: Division by zero is not allowed."
-    return num1 / num2
+    return numero_1 / numero_2
 
-def exponent(base, exp):
+def exponent(base, exponente):
     """ Deuelve la potencia del exponente. """
-    return base ** exp
+    return base ** exponente
 
-def modulo(num1, num2):
+def modulo(numero_1, numero_2):
     """ Devuelve el residuo de dividir numero 1 entre el numero 2.
     rectifica que numero 2 no sea cero. """
-    if num2 == 0:
+    if numero_2 == 0:
         return "Error: Modulo by zero is not allowed."
-    return num1 % num2
+    return numero_1 % numero_2
 
-def floor_divide(num1, num2):
+def floor_divide(numero_1, numero_2):
     """ Devuelve el entero más grande menor o igual al resultado de 
     dividir numero 1 entre el numero 2. Rectifica que el numero 2 es cero. """
-    if num2 == 0:
+    if numero_2 == 0:
         return "Error: Division by zero is not allowed."
-    return num1 // num2
+    return numero_1 // numero_2
 
-def absolute(num):
+def absolute(numero):
     """ Devuelve el valor absoluto del numero. """
-    return abs(num)
+    return abs(numero)
 
 
